7c00f174d9c790bb450108ca142dbf4fc1059d06
Carlos Gil <carlos.gil@betterment.com>
2017-04-27 17:41:25 -0400

    Tracking seen words

--- generate.py
+++ generate.py
@@ -9,16 +9,35 @@ def create_common_words():
                 f.write('{}\n'.format(word))
 
 
-def get_random_phrases(n=300, max_size=12):
-    with open('common_words.txt', 'r') as f:
-        words = f.read().split('\n')
-        count = 0
-        while count < n:
-            a = random.randint(0, len(words) - 1)
-            b = random.randint(0, len(words) - 1)
-            phrase = '{}{}.com'.format(words[a], words[b])
-            if len(phrase) <= max_size:
-                print phrase
-                count += 1
+def get_seen_words():
+    with open('seen.txt', 'r') as f:
+        return set(f.read().split('\n'))
 
-get_random_phrases(450, 11)
+def get_common_words():
+    with open('common_words.txt', 'r') as f:
+        return f.read().split('\n')
+
+def add_seen_word(word):
+    with open('seen.txt', 'a') as f:
+        f.write('{}\n'.format(word))
+
+
+def get_random_phrases(n=300, max_size=12):
+    max_fails = 500000
+    words = get_common_words()
+    seen = get_seen_words()
+    count = 0
+    fails = 0
+    while count < n and fails < max_fails:
+        a = random.randint(0, len(words) - 1)
+        b = random.randint(0, len(words) - 1)
+        phrase = '{}{}'.format(words[a], words[b]).strip()
+        if len(phrase) <= max_size and len(phrase) > 4 and phrase not in seen:
+            seen.add(phrase)
+            add_seen_word(phrase)
+            print '{}.com'.format(phrase)
+            count += 1
+        else:
+            fails += 1
+
+get_random_phrases(450, 8)
